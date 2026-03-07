3fdc48ede6cdd33d968236717059afdd4d70f957
otgursop <otgursop@git.com>
2026-03-07 18:23:31 +0500

    idk updated

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -19,8 +19,6 @@ return {
       pattern = langs,
       callback = function()
         vim.treesitter.start()
-        -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-        -- vim.wo.foldmethod = 'expr'
         vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
       end,
     })
